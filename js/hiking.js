c1a275cd62c0f9bd0ef1a5b17f59bc3e1bfbe320
JenBrook <jenbrook375@hotmail.com>
2019-05-23 18:16:29 -0700

    added individual trails thru index position

--- js/hiking.js
+++ js/hiking.js
@@ -31,23 +31,57 @@ request.onload = function() {
 
     // taking the relevant info from the objects, then calling the id's 
     // inside the HTML elements and setting the object info on the elements
-    
+
+    // trail from index 0
     var img = trails[0].imgMedium;// grab small/med size img of trail
     document.getElementById("trail-img").src = img;
-
+  
     var name = trails[0].name; // grab name of trail
     document.getElementById("trail-name").innerHTML = name;
-
+  
     var location = trails[0].location; // grab location of trail
     document.getElementById('trail-loc').innerHTML = location;
-
+  
     var summary = trails[0].summary; // grab summary
     document.getElementById('trail-desc').innerHTML = summary;
-
+  
     var trailUrl = data.trails[0].url; // grab link to trail
-    document.getElementById('trail-url').innerHTML = trailUrl;
+    document.getElementById('trail-url').href = trailUrl;
 
-    console.log(img, name, summary, trailUrl); // display data for test purposes
+
+    //trail from index 1
+    var img = trails[1].imgMedium;// grab small/med size img of trail
+    document.getElementById("trail-img-1").src = img;
+  
+    var name1 = trails[1].name; // grab name of trail
+    document.getElementById("trail-name-1").innerHTML = name1;
+  
+    var location = trails[1].location; // grab location of trail
+    document.getElementById('trail-loc-1').innerHTML = location;
+  
+    var summary = trails[1].summary; // grab summary
+    document.getElementById('trail-desc-1').innerHTML = summary;
+  
+    var trailUrl = data.trails[1].url; // grab link to trail
+    document.getElementById('trail-url-1').href = trailUrl;
+    
+    // trail from index 2
+    var img2 = trails[2].imgMedium;// grab small/med size img of trail
+    document.getElementById("trail-img-2").src = img2;
+  
+    var name2 = trails[2].name; // grab name of trail
+    document.getElementById("trail-name-2").innerHTML = name2;
+  
+    var location2 = trails[2].location; // grab location of trail
+    document.getElementById('trail-loc-2').innerHTML = location2;
+  
+    var summary2 = trails[2].summary; // grab summary
+    document.getElementById('trail-desc-2').innerHTML = summary2;
+  
+    var trailUrl2 = data.trails[2].url; // grab link to trail
+    document.getElementById('trail-url-2').href = trailUrl2;
+
+
 
   };
 
