0f29b5c61433c91b2f226b6f13a954f5240c69ea
Gabriela T <gtrujillo15@gmail.com>
2019-06-02 21:39:31 -0700

    completed header title and basic footer. started on JS for mouseover event.

---
+++ js/biking.js
@@ -0,0 +1,5 @@
+var cardColor = document.getElementsByClassName("card").addEventListener("mouseover", mouseOver);
+
+function mouseOver() {
+    document.getElementsByClassName("card").style.border_color = "red";
+}
