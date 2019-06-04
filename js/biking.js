9610f11ec7c48ad08011fc2ac1e3fe61bede1292
Gabriela T <gtrujillo15@gmail.com>
2019-06-03 19:37:20 -0700

    pulled hiking trails info and added modal

--- js/biking.js
+++
@@ -1,5 +0,0 @@
-var cardColor = document.getElementsByClassName("card").addEventListener("mouseover", mouseOver);
-
-function mouseOver() {
-    document.getElementsByClassName("card").style.border_color = "red";
-}
