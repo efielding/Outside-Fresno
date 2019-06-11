7e7e825ea662ac47d1dda3caa3d33b86ffac3e9a
Gabriela T <gtrujillo15@gmail.com>
2019-06-10 20:21:22 -0700

    worked on module

--- js/module.js
+++ js/module.js
@@ -1,7 +1,13 @@
-var name = document.getElementById("nameinput");
-var email = document.getElementById("emailinput");
-var submit = document.getElementById("submitbutton");
+// function storeInfo() {
+//     let name = document.getElementById("nameinput").Value;
+//     let email = document.getElementById("email").Value;
+//     localStorage.setItem(name, name);
+// }
 
-submit.addEventListener("click", function() {
-    name
-})
+
+let user = name =>{
+    console.log(localStorage.getItem('nameinput'));
+    if (!localStorage.get('nameinput')){
+    localStorage.setItem('nameinput', name);
+    return name;
+}}
