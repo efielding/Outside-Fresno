61616c683fa454a7d7751f21fcdfe6bbbbdab876
JenBrook <jenbrook375@hotmail.com>
2019-06-10 20:05:32 -0700

    worked on drop-down nav

--- js/hiking.js
+++ js/hiking.js
@@ -1,3 +1,8 @@
+function openNav(){
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+// this is the code for the 'hiking project' api
 // apiKey 200468098-d3b92bb22cd80b7ec7b0b1c7d30741cd
 // lon: -119.78
 // lat: 36.74
@@ -11,9 +16,6 @@ var results = '&maxResults=50'; // #of results
 var apiKey = '&key=200468098-d3b92bb22cd80b7ec7b0b1c7d30741cd'; // apiKey
 var url = getTrails + latitude + longitude + distance + sort + results + apiKey; //concat variables to url
 console.log(url); // display url for test purposes
-
-
-
 
 var request = new XMLHttpRequest()
 
@@ -181,34 +183,34 @@ request.onload = function () {
 request.send()
 
 
-// //this is the code to add the current weather- haven't decided to use it on hiking page yet
+// //this is the code to add the current weather
 // // variables for each element of api call and api key
-// var api = 'https://api.openweathermap.org/data/2.5/weather?q='; // api endpoint
-// var city = 'Fresno';
-// var apiKey = '&appid=fe9a422805a990c7b90f9de0efcc182b'; // api key assigned to account
-// var units = '&units=imperial'; // express temps in farenheit
-// var url = api + city + units + apiKey; // concatenate api elements into url 
-// console.log(url); // display url
+var api = 'https://api.openweathermap.org/data/2.5/weather?q='; // api endpoint
+var city = 'Fresno';
+var apiKey = '&appid=fe9a422805a990c7b90f9de0efcc182b'; // api key assigned to account
+var units = '&units=imperial'; // express temps in farenheit
+var url = api + city + units + apiKey; // concatenate api elements into url 
+console.log(url); // display url
 
-// var request = new XMLHttpRequest()
+var request = new XMLHttpRequest()
 
 
-// request.open('GET', url, true)
-// request.onload = function() {
-//   // Begin accessing JSON data here
-//   var data = JSON.parse(this.response)
+request.open('GET', url, true)
+request.onload = function() {
+  // Begin accessing JSON data here
+  var data = JSON.parse(this.response)
   
 
-//   var icon = 'https://openweathermap.org/img/w/' + data.weather[0].icon + 
-//     '.png';// url for icon and its place in the json obj
-//   var iconImage = document.getElementById('icon').src = icon; // set the img on the #icon element
+  var icon = 'https://openweathermap.org/img/w/' + data.weather[0].icon + 
+    '.png';// url for icon and its place in the json obj
+  var iconImage = document.getElementById('icon').src = icon; // set the img on the #icon element
 
-//   var weather = data.weather[0].description; // grab weather description
-//   document.getElementById('weather').innerHTML = weather; // set var weather on #weather element
+  var weather = data.weather[0].description; // grab weather description
+  document.getElementById('weather').innerHTML = weather; // set var weather on #weather element
 
-//   var temp = data.main.temp; // grap temp data
-//   var formattedTemp = Math.round(temp); // round temp up/down to int
-//     console.log(formattedTemp); // display temp as int
-//   document.getElementById('temp').innerHTML = formattedTemp + '°'; // display temperature as an int in #temp
-// }
-// request.send()
+  var temp = data.main.temp; // grap temp data
+  var formattedTemp = Math.round(temp); // round temp up/down to int
+    console.log(formattedTemp); // display temp as int
+  document.getElementById('temp').innerHTML = formattedTemp + '°'; // display temperature as an int in #temp
+}
+request.send()
