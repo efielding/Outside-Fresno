40fe522ac5b12774819fe4c04a248819ee8010c6
Gabriela T <gtrujillo15@gmail.com>
2019-06-12 22:40:46 -0700

    modal can now save name and email to local storage. would like to have form return if value is <1 for either field and to clear when fields are entered

--- js/module.js
+++ js/module.js
@@ -1,13 +1,8 @@
-// function storeInfo() {
-//     let name = document.getElementById("nameinput").Value;
-//     let email = document.getElementById("email").Value;
-//     localStorage.setItem(name, name);
-// }
-
-
-let user = name =>{
-    console.log(localStorage.getItem('nameinput'));
-    if (!localStorage.get('nameinput')){
-    localStorage.setItem('nameinput', name);
-    return name;
-}}
+var formSubmit = document.getElementById('submit-info');
+formSubmit.addEventListener('click', function(event) {
+    event.preventDefault();
+    var nameSet = document.getElementById('name-input').value;
+    var emailSet = document.getElementById('email-input').value;
+    localStorage.setItem('name', nameSet);
+    localStorage.setItem('email', emailSet);
+})
