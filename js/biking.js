77f6e18dd38451204d4485628c42d97e28d75a6f
Gabriela T <gtrujillo15@gmail.com>
2019-06-22 18:36:09 -0700

    created save buttons and working on js to toggle saved

--- js/biking.js
+++ js/biking.js
@@ -24,24 +24,15 @@ document.getElementById("test").innerHTML = welcome;
 
 // START OF WATCH LIST
 
-var saveWatch = document.getElementById('save');
-saveWatch.addEventListener('click', function(event)) {
-  document.getElementById('save').classList.toggle("Saved to Watch List");
-}
-
-// code for modal to grab name and email and save to local storage
-var formSubmit = document.getElementById('submit-info');
-formSubmit.addEventListener('click', function(event) {
-    event.preventDefault();
-    // grabbing name and email from the form in html
-    var nameSet = document.getElementById('name-input').value;
-    var emailSet = document.getElementById('email-input').value;
-    // saving to local storage and naming key
-    localStorage.setItem('name', nameSet);
-    localStorage.setItem('email', emailSet);
-    // grabbing name and calling div id to insert into div
-    document.getElementById("test").innerHTML = localStorage.getItem("name");
+var savedList = document.getElementById("save");
+savedList.addEventListener('click', function() {
+  if (savedList.innerHTML == "Save to Watchlist") {
+    savedList.innerHTML = "Saved";
+  } else {
+    savedList.innerHTML = "Save to Watchlist";
+  }
 })
+
 
 // API FOR WEATHER
 
